b0b8ba9470a18333acb0436059f3e0ae358f011e
Uriel <carrillouriel@gmail.com>
2017-02-18 14:58:01 -0700

    Finished DatabaseHelper class

--- MyORMLiteExample/app/src/main/java/com/androidatc/myormliteexample/DatabaseHelper.java
+++ MyORMLiteExample/app/src/main/java/com/androidatc/myormliteexample/DatabaseHelper.java
@@ -26,4 +26,84 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
         super(context,DATABASE_NAME,null,DATABASE_VERSION);
         mContext=context;
     }
+
+    public Dao<Person,Long> getDao() throws SQLException{
+        if(mSimpleDao==null)
+        {
+            mSimpleDao=getDao(Person.class);
+        }
+        return mSimpleDao;
+    }
+    /*
+    Fech all the Person records in the database
+    @return List of Person records found in the database
+     */
+    public List<Person> GetData()
+    {
+        DatabaseHelper helper = new DatabaseHelper(mContext);
+        try{
+            Dao<Person,Long> simpleDao = helper.getDao();
+            return simpleDao.queryForAll();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    /*
+    Add the specified Person object to the database
+    @param person Person object to persist to the database
+    @return
+        The number of rows updated in the database. This should be 1.
+     */
+    public int addData(Person person)
+    {
+        try {
+            Dao<Person,Long> dao = getDao();
+            return dao.create(person);
+        }catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+    /*
+    Delete all Person records in the database
+     */
+    public void deleteAll()
+    {
+        try{
+            Dao<Person,Long> dao = getDao();
+            List<Person> list = dao.queryForAll();
+            dao.delete(list);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource)
+    {
+        try {
+            Log.i(DatabaseHelper.class.getName(),"onCreate");
+            TableUtils.createTable(connectionSource,Person.class);
+        }catch (SQLException e)
+        {
+            Log.e(DatabaseHelper.class.getName(),"Cannot create database",e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion)
+    {
+        try{
+            Log.i(DatabaseHelper.class.getName(),"onUpgrade");
+            TableUtils.dropTable(connectionSource,Person.class,true);
+            onCreate(db,connectionSource);
+        }catch (SQLException e)
+        {
+            Log.e(DatabaseHelper.class.getName(),"Cannot drop databases",e);
+            throw new RuntimeException(e);
+        }
+    }
 }
